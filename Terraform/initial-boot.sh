2ffd50e7bc0b49e71829181d1f145191434f2e9a
sachinshrma <36964363+sachinshrma@users.noreply.github.com>
2020-04-08 18:13:28 +0530

    Update and rename initial-boot.sh to initial-boot.txt

--- Terraform/initial-boot.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo docker pull sachinshrma/petclinic:1.0.0
-sudo docker run -d --name webapp sachinshrma/petclinic:1.0.0
