acc50e3da4f5756a1a6881b796d5c1a3d7895a1c
sachinshrma <36964363+sachinshrma@users.noreply.github.com>
2020-04-08 12:05:26 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -76,7 +76,7 @@ resource "azurerm_virtual_machine" "virtual-machine" {
   location              = "${var.location}"
   resource_group_name   = "${azurerm_resource_group.resource-group.name}"
   network_interface_ids = ["${azurerm_network_interface.nic.id}"]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Basic_A1"
 
   storage_image_reference {
     publisher = "Canonical"
